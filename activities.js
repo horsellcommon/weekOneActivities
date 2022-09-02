7d5ff6724a5052da1d41b3f6f19549f3f79a85f8
horsellcommon <crawfordj716@gmail.com>
2022-09-02 14:49:12 +0100

    Fixed formatting

--- activities.js
+++ activities.js
@@ -20,70 +20,70 @@ console.log(pet.drink());
 
 let randomNumber = [];
 const generate = () => {
-    let generatedNumber = Math.round(Math.random() * 30)
-        if (randomNumber.length == 6){
-            console.log(`Your six numbers that are divisible by 7 are ${randomNumber}`)
-        } else if (generatedNumber % 7 == 0){
-            randomNumber.push(generatedNumber)
-            console.log(generatedNumber)
-            generate()
-        } else if (generatedNumber % 7 != 0){
-            console.log("Number is not divisible by 7")  
-            generate()          
-        } 
-}
+  let generatedNumber = Math.round(Math.random() * 30);
+  if (randomNumber.length == 6) {
+    console.log(`Your six numbers that are divisible by 7 are ${randomNumber}`);
+  } else if (generatedNumber % 7 == 0) {
+    randomNumber.push(generatedNumber);
+    console.log(generatedNumber);
+    generate();
+  } else if (generatedNumber % 7 != 0) {
+    console.log("Number is not divisible by 7");
+    generate();
+  }
+};
 
-generate()
+generate();
 
 // sayHi function
 
 person = {
-    name: "Mick",
-    age: 30,
-    sayHi(){
-        return `Hello, my name is ${person.name}`
-    }
-}
+  name: "Mick",
+  age: 30,
+  sayHi() {
+    return `Hello, my name is ${person.name}`;
+  },
+};
 
-console.log(person.sayHi())
+console.log(person.sayHi());
 
 // Generate and log 6 random numbers between 1-50
 
 const consoleLogger = () => {
-    for (let t = 0; t < 6; t++){
-        let thisVariable = Math.round(Math.random() * 50)
-        console.log(thisVariable)
-    }
-}
-consoleLogger()
+  for (let t = 0; t < 6; t++) {
+    let thisVariable = Math.round(Math.random() * 50);
+    console.log(thisVariable);
+  }
+};
+consoleLogger();
 
 // Program that counts down from 9 to 0
 
 const countDown = () => {
-    for (let increment = 9; increment > -1; increment--){
-        console.log(increment)
-    }
-}
-countDown()
+  for (let increment = 9; increment > -1; increment--) {
+    console.log(increment);
+  }
+};
+countDown();
 
 // Storing data - breakfast-lunch-dinner
 
 const foodLogger = (mealOne, mealTwo, mealThree) => {
-    breakfast = mealOne
-    lunch = mealTwo
-    dinner = mealThree
-}
+  breakfast = mealOne;
+  lunch = mealTwo;
+  dinner = mealThree;
+};
 
-foodLogger("Toast", "Soup", "Pasta")
-console.log(`Breakfast: ${breakfast}`)
-console.log(`Lunch: ${lunch}`)
-console.log(`Dinner: ${dinner}`)
+foodLogger("Toast", "Soup", "Pasta");
+console.log(`Breakfast: ${breakfast}`);
+console.log(`Lunch: ${lunch}`);
+console.log(`Dinner: ${dinner}`);
 
 // Film Array - list favourite films then add 2 using a method and then loop to cycle through array
 
 let filmArray = ["The Thing", "A Clockwork Orange", "Blade Runner"];
 
-filmArray.push("The Lighthouse", "Hitchhiker's Guide To The Galaxy")
+filmArray.push("The Lighthouse", "Hitchhiker's Guide To The Galaxy");
 
 for (let i = 0; i < filmArray.length; i++) {
   console.log(filmArray[i]);
